529be7047cfca6d30b78ffaad1f441643460b04e
Andrew <sawosinandreww@mail.ru>
2023-03-25 11:15:50 +0300

    Задача 2

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2
+Console.WriteLine("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 > Number2)
+{
+    if (Number1 > Number3)
+    {
+        Console.WriteLine("Максимальное число: " + Number1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + Number3);
+    }
+}
+
+else if (Number2 > Number3)
+{
+    Console.WriteLine("Максимальное число: " + Number2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + Number3);
+}

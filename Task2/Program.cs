4ebbc66c553140105d406776e72f3d9cee22cb83
Andrew <sawosinandreww@mail.ru>
2023-03-25 11:17:32 +0300

    Задача 4

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Number = 1;
+
+
+while(Number <= number)
+    {
+ if(Number % 2 ==0)
+ Console.Write(Number + ", ");
+ Number ++;
+    }
